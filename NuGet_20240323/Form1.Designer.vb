d03b9689bddb06ecef8eec8bcd5272643c82d735
shi-you-ta <shota_hakamada1993@outlook.com>
2024-04-06 16:06:29 +0900

    CSV出力2ボタンを追加し、最終行に列毎の合計セルを追加仕様

---
+++ NuGet_20240323/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBoxInputCSVFileName = New TextBox()
+        TextBoxOutputCSVFileName = New TextBox()
+        ButtonCSVRead = New Button()
+        ButtonCsvWrite = New Button()
+        DataGridViewCsv = New DataGridView()
+        OpenFileDialogCsv = New OpenFileDialog()
+        SaveFileDialogCsv = New SaveFileDialog()
+        ButtonCsvWrite2 = New Button()
+        TextBoxOutputCSVFileName2 = New TextBox()
+        CType(DataGridViewCsv, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TextBoxInputCSVFileName
+        ' 
+        TextBoxInputCSVFileName.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBoxInputCSVFileName.Location = New Point(12, 9)
+        TextBoxInputCSVFileName.Name = "TextBoxInputCSVFileName"
+        TextBoxInputCSVFileName.Size = New Size(612, 27)
+        TextBoxInputCSVFileName.TabIndex = 0
+        ' 
+        ' TextBoxOutputCSVFileName
+        ' 
+        TextBoxOutputCSVFileName.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBoxOutputCSVFileName.Location = New Point(12, 424)
+        TextBoxOutputCSVFileName.Name = "TextBoxOutputCSVFileName"
+        TextBoxOutputCSVFileName.Size = New Size(612, 27)
+        TextBoxOutputCSVFileName.TabIndex = 0
+        ' 
+        ' ButtonCSVRead
+        ' 
+        ButtonCSVRead.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        ButtonCSVRead.Location = New Point(643, 6)
+        ButtonCSVRead.Name = "ButtonCSVRead"
+        ButtonCSVRead.Size = New Size(154, 33)
+        ButtonCSVRead.TabIndex = 1
+        ButtonCSVRead.Text = "CSV取得"
+        ButtonCSVRead.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonCsvWrite
+        ' 
+        ButtonCsvWrite.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        ButtonCsvWrite.Location = New Point(630, 421)
+        ButtonCsvWrite.Name = "ButtonCsvWrite"
+        ButtonCsvWrite.Size = New Size(154, 32)
+        ButtonCsvWrite.TabIndex = 1
+        ButtonCsvWrite.Text = "CSV出力"
+        ButtonCsvWrite.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridViewCsv
+        ' 
+        DataGridViewCsv.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        DataGridViewCsv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCsv.Location = New Point(12, 46)
+        DataGridViewCsv.Name = "DataGridViewCsv"
+        DataGridViewCsv.RowHeadersWidth = 51
+        DataGridViewCsv.Size = New Size(785, 361)
+        DataGridViewCsv.TabIndex = 2
+        ' 
+        ' OpenFileDialogCsv
+        ' 
+        OpenFileDialogCsv.FileName = "*.csv"
+        OpenFileDialogCsv.InitialDirectory = ".\"
+        ' 
+        ' SaveFileDialogCsv
+        ' 
+        SaveFileDialogCsv.Filter = "CSVファイル|*.csv|すべてのファイル|*.*"
+        SaveFileDialogCsv.InitialDirectory = ".\"
+        ' 
+        ' ButtonCsvWrite2
+        ' 
+        ButtonCsvWrite2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        ButtonCsvWrite2.Location = New Point(630, 458)
+        ButtonCsvWrite2.Name = "ButtonCsvWrite2"
+        ButtonCsvWrite2.Size = New Size(154, 29)
+        ButtonCsvWrite2.TabIndex = 3
+        ButtonCsvWrite2.Text = "CSV出力2"
+        ButtonCsvWrite2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBoxOutputCSVFileName2
+        ' 
+        TextBoxOutputCSVFileName2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBoxOutputCSVFileName2.Location = New Point(12, 460)
+        TextBoxOutputCSVFileName2.Name = "TextBoxOutputCSVFileName2"
+        TextBoxOutputCSVFileName2.Size = New Size(612, 27)
+        TextBoxOutputCSVFileName2.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(809, 532)
+        Controls.Add(TextBoxOutputCSVFileName2)
+        Controls.Add(ButtonCsvWrite2)
+        Controls.Add(DataGridViewCsv)
+        Controls.Add(ButtonCsvWrite)
+        Controls.Add(ButtonCSVRead)
+        Controls.Add(TextBoxOutputCSVFileName)
+        Controls.Add(TextBoxInputCSVFileName)
+        Name = "Form1"
+        Text = "CSVの読み書き"
+        CType(DataGridViewCsv, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBoxInputCSVFileName As TextBox
+    Friend WithEvents TextBoxOutputCSVFileName As TextBox
+    Friend WithEvents ButtonCSVRead As Button
+    Friend WithEvents ButtonCsvWrite As Button
+    Friend WithEvents DataGridViewCsv As DataGridView
+    Friend WithEvents OpenFileDialogCsv As OpenFileDialog
+    Friend WithEvents SaveFileDialogCsv As SaveFileDialog
+    Friend WithEvents ButtonCsvWrite2 As Button
+    Friend WithEvents TextBoxOutputCSVFileName2 As TextBox
+
+End Class
---
+++ NuGet_20240323/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.IO       'Fileを使うのに必要
+Imports System.Text     'Encoding（テキストやデータを倍と列に変換する方法）を使うのに必要
+Imports Csv
+
+
+Public Class Form1
+    '内部でデータを保持するテーブルを用意します
+    '2つのイベントハンドラーのメソッドから呼び出されるのでクラスで定義
+    Dim dataTable As New DataTable()
+
+    'csv取得ボタンクリック時の処理
+    Private Sub ButtonCSVRead_Click(sender As Object, e As EventArgs) Handles ButtonCSVRead.Click
+        'ファイルを開くウィンドウでCSVファイルを選択し、OKボタンを押した時
+        If (OpenFileDialogCsv.ShowDialog() = DialogResult.OK) Then
+            'TextBoxInputCSVFileNameのtextにCSVファイル名を反映させる
+            TextBoxInputCSVFileName.Text = OpenFileDialogCsv.FileName
+            'ファイルの全内容を文字列に読み込む
+            '日本語が読み込めるように文字はUTF-8でエンコードします。
+            Dim csv = File.ReadAllText(OpenFileDialogCsv.FileName, Encoding.UTF8)
+            dataTable.TableName = "CSVTable"              '内部でテーブルを生成します
+            dataTable.Columns.Clear()                     '内部テーブルのヘッダーを初期化
+            dataTable.Clear()                             '内部テーブルのデータを初期化
+
+            'CSVのヘッダー部分のデータを取得し、内部のテーブルカラムに設定
+            'csvから1行取得し、結果をline変数に入れる
+            For Each line In CsvReader.ReadFromText(csv)
+                '1行分のデータのヘッダーの情報を取り出す
+                For Each item In line.Headers
+                    dataTable.Columns.Add(item)         '内部テーブルのカラムに設定
+                Next
+                Exit For                                '2列目以降のデータもカラム名は同じなのでCSV読み込み終了
+            Next
+
+            '読み込んだcsvデータを内部テーブルに割り当てる
+            'もう一度csvから1行取得し、結果をline変数に入れる
+            For Each line In CsvReader.ReadFromText(csv)
+                dataTable.Rows.Add(line.Values)         '1レコード分まとめて設定
+            Next
+            '表示用のDataGridViewに内部のテーブルを割り当てる
+            DataGridViewCsv.DataSource = dataTable
+        End If
+
+    End Sub
+
+    'CSV出力ボタンクリック時の処理
+    Private Sub ButtonCsvWrite_Click(sender As Object, e As EventArgs) Handles ButtonCsvWrite.Click
+        'ファイルを保存するウィンドウで選んだCSVファイルを選択し、OKボタンをクリックした時
+        If (SaveFileDialogCsv.ShowDialog() = DialogResult.OK) Then
+            'TextBoxOutputCSVFileNameのtextにCSVファイル名を反映させる
+            TextBoxOutputCSVFileName.Text = SaveFileDialogCsv.FileName
+            'headerという変数に内部のテーブルのカラム名を設定
+            Dim header(dataTable.Columns.Count - 1) As String
+            'カラムの数だけループしてカラムのデータを設定
+            For i = 0 To dataTable.Columns.Count - 1
+                header(i) = dataTable.Columns(i).ColumnName
+            Next
+
+            'newLineという変数に内部のテーブルを表のイメージ（2次元配列）で設定
+            Dim newLine(dataTable.Rows.Count - 1)() As String
+            'データの数分ループして、データを取得
+            For i = 0 To dataTable.Rows.Count - 1
+                ReDim newLine(i)(dataTable.Columns.Count - 1)
+                '該当するカラム、列の値を内部のテーブルから、newLineに設定
+                For j As Integer = 0 To dataTable.Columns.Count - 1
+                    'Nothing（DBの値がnull）の場合、""としてnewLineを設定
+                    newLine(i)(j) = IIf(Convert.IsDBNull(dataTable.Rows(i)(j)), "",
+                                        dataTable.Rows(i)(j))
+                Next
+            Next
+            'データからCSV形式の文字列を生成
+            Dim outcsv = CsvWriter.WriteToText(header, newLine)
+            'FileNameという名前で、outcsvの値を保存します。文字はUTF8でエンコードします。
+            File.WriteAllText(SaveFileDialogCsv.FileName, outcsv, Encoding.UTF8)
+        End If
+    End Sub
+
+    'CSV出力2ボタンクリック時の処理
+    Private Sub ButtonCsvWrite2_Click(sender As Object, e As EventArgs) Handles ButtonCsvWrite2.Click
+        'ファイルを保存するウィンドウで選んだCSVファイルを選択しOKをクリックする場合
+        If (SaveFileDialogCsv.ShowDialog() = DialogResult.OK) Then
+            'TextBoxOutputCSVFileName2のtextにCSVファイル名を反映させる
+            TextBoxOutputCSVFileName2.Text = SaveFileDialogCsv.FileName
+            'header2という変数に内部のテーブルのカラム名を設定
+            Dim header2(dataTable.Columns.Count - 1) As String
+            'カラムの数だけループしてカラムのデータを設定
+            For i = 0 To dataTable.Columns.Count - 1
+                header2(i) = dataTable.Columns(i).ColumnName
+            Next
+
+            'newLine2という変数に内部のテーブルを表のイメージ（2次元配列）で設定
+            Dim newLine2(dataTable.Rows.Count)() As String
+
+            'データの数分ループしてデータを取得
+            For i = 0 To dataTable.Rows.Count - 1
+                ReDim newLine2(i)(dataTable.Columns.Count - 1)
+                For j = 0 To dataTable.Columns.Count - 1
+                    'Nothing（DBの値がnullの場合）、""としてnewLine2を設定
+                    newLine2(i)(j) = IIf(Convert.IsDBNull(dataTable.Rows(i)(j)), "", dataTable.Rows(i)(j))
+                Next
+            Next
+
+            '各列の合計値を格納する配列
+            Dim sumRow(dataTable.Columns.Count - 1) As String
+            For i As Integer = 0 To dataTable.Columns.Count - 1
+                Dim totalValue As Integer = 0
+                For j As Integer = 0 To dataTable.Rows.Count - 1
+                    '各列の合計値を計算
+                    If Not Convert.IsDBNull(dataTable.Rows(j)(i)) Then
+                        totalValue += Convert.ToInt32(dataTable.Rows(j)(i))
+                    End If
+                Next
+                sumRow(i) = totalValue.ToString()
+            Next
+            newLine2(dataTable.Rows.Count) = sumRow
+
+            'データからCSV形式の文字列を生成
+            Dim outcsv2 = CsvWriter.WriteToText(header2, newLine2)
+            'FileNameという名前で、outcsvの値を保存する。文字はUTF-8でエンコード
+            File.WriteAllText(SaveFileDialogCsv.FileName, outcsv2, Encoding.UTF8)
+        End If
+    End Sub
+
+End Class
